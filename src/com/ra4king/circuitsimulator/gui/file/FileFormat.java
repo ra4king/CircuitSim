da44f427cdd33f3cbe199d1a23196f413fc887ff
Roi Atalla <roi@roiatalla.com>
2017-06-11 18:51:17 -0400

    Bumping to v1.3

--- src/com/ra4king/circuitsimulator/gui/file/FileFormat.java
+++ src/com/ra4king/circuitsimulator/gui/file/FileFormat.java
@@ -32,7 +32,7 @@ import javafx.util.Pair;
 public class FileFormat {
 	private static final Gson GSON;
 	
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.3";
 	
 	static {
 		GSON = new GsonBuilder().setPrettyPrinting().create();

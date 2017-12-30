1ab74bd56e8821631d058210bf12da49f3dac869
Roi Atalla <ra4king@gmail.com>
2017-12-29 20:56:41 -0500

    Bump to 1.5.3.

--- src/com/ra4king/circuitsim/gui/file/FileFormat.java
+++ src/com/ra4king/circuitsim/gui/file/FileFormat.java
@@ -32,7 +32,7 @@ import javafx.util.Pair;
 public class FileFormat {
 	private static final Gson GSON;
 	
-	public static final String VERSION = "1.5.2";
+	public static final String VERSION = "1.5.3";
 	
 	static {
 		GSON = new GsonBuilder().setPrettyPrinting().create();

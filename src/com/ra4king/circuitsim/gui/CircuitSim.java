161c024bcc00551c2ddb8cb8b27410c0019d9778
Roi Atalla <roi@roiatalla.com>
2018-01-05 02:44:05 -0800

    Bumped version to 1.5.5.

--- src/com/ra4king/circuitsim/gui/CircuitSim.java
+++ src/com/ra4king/circuitsim/gui/CircuitSim.java
@@ -118,7 +118,7 @@ import javafx.util.Pair;
  * @author Roi Atalla
  */
 public class CircuitSim extends Application {
-	public static final String VERSION = "1.5.4";
+	public static final String VERSION = "1.5.5";
 	
 	private static boolean mainCalled = false;
 	

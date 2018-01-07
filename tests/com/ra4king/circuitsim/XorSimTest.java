d7603abaf3998a8f1c8f25b19c0af50e08621e60
Patrick Tam <pjztam@gmail.com>
2018-01-07 04:54:08 -0800

    added example XOR autograder

---
+++ tests/com/ra4king/circuitsim/XorSimTest.java
@@ -0,0 +1,160 @@
+package com.ra4king.circuitsim;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Optional;
+
+import com.ra4king.circuitsim.gui.CircuitBoard;
+import com.ra4king.circuitsim.gui.CircuitSim;
+import com.ra4king.circuitsim.gui.peers.SubcircuitPeer;
+import com.ra4king.circuitsim.gui.peers.memory.RAMPeer;
+import com.ra4king.circuitsim.gui.peers.wiring.PinPeer;
+import com.ra4king.circuitsim.simulator.CircuitState;
+import com.ra4king.circuitsim.simulator.Simulator;
+import com.ra4king.circuitsim.simulator.WireValue;
+import com.ra4king.circuitsim.simulator.components.Subcircuit;
+import com.ra4king.circuitsim.simulator.components.memory.RAM;
+import com.ra4king.circuitsim.simulator.components.wiring.Clock;
+import com.ra4king.circuitsim.simulator.components.wiring.Pin;
+
+/**
+ * @author Patrick Tam
+ */
+public class XorSimTest {
+    public static void main(String[] args) {
+
+        try {
+            CircuitSim simulator = new CircuitSim(false);
+
+            simulator.loadCircuits(new File("xor.sim"));
+
+            Simulator sim = simulator.getSimulator();
+
+            Map<String, CircuitBoard> circuitBoards = simulator.getCircuitBoards();
+
+            CircuitBoard xor = circuitBoards.get("xor");
+
+            Optional<Pin> ain =
+                xor.getComponents().stream()
+                    .filter(c -> c instanceof PinPeer)
+                    .map(c -> ((PinPeer)c).getComponent())
+                    .filter(p -> p.isInput() && p.getName().equals("a"))
+                    .findFirst();
+
+            if(!ain.isPresent()) {
+                throw new IllegalStateException("a input does not exist in circuit");
+            }
+
+            Optional<Pin> bin =
+                    xor.getComponents().stream()
+                            .filter(c -> c instanceof PinPeer)
+                            .map(c -> ((PinPeer)c).getComponent())
+                            .filter(p -> p.isInput() && p.getName().equals("b"))
+                            .findFirst();
+
+            if(!bin.isPresent()) {
+                throw new IllegalStateException("b input does not exist in circuit");
+            }
+
+            Optional<Pin> cout =
+                    xor.getComponents().stream()
+                            .filter(c -> c instanceof PinPeer)
+                            .map(c -> ((PinPeer)c).getComponent())
+                            .filter(p -> !p.isInput() && p.getName().equals("c"))
+                            .findFirst();
+
+            if(!cout.isPresent()) {
+                throw new IllegalStateException("c output does not exist in circuit");
+            }
+
+            final WireValue ONE = WireValue.of(1, 1);
+            final WireValue ZERO = WireValue.of(0, 1);
+
+            // 0^0 = 0
+            ain.get().setValue(xor.getCurrentState(), ONE);
+            bin.get().setValue(xor.getCurrentState(), ZERO);
+            sim.stepAll();
+            assert xor.getCurrentState().getLastReceived(cout.get().getPort(Pin.PORT)).getValue() == 1;
+
+            // 0^1 = 1
+            ain.get().setValue(xor.getCurrentState(), ZERO);
+            bin.get().setValue(xor.getCurrentState(), ONE);
+            sim.stepAll();
+            assert xor.getCurrentState().getLastReceived(cout.get().getPort(Pin.PORT)).getValue() == 1;
+
+
+            // 1^0 = 1
+            ain.get().setValue(xor.getCurrentState(), ONE);
+            bin.get().setValue(xor.getCurrentState(), ZERO);
+            sim.stepAll();
+            assert xor.getCurrentState().getLastReceived(cout.get().getPort(Pin.PORT)).getValue() == 1;
+
+
+            // 1^1 = 0
+            ain.get().setValue(xor.getCurrentState(), ONE);
+            bin.get().setValue(xor.getCurrentState(), ONE);
+            sim.stepAll();
+            assert xor.getCurrentState().getLastReceived(cout.get().getPort(Pin.PORT)).getValue() == 0;
+
+
+
+
+//
+//            if(openWindow) {
+//                Clock.tick(simulator.getSimulator());
+//
+//                Thread.sleep(1000);
+//            }
+//
+//            CircuitBoard memory = circuitBoards.get("Memory");
+//            if(memory == null) {
+//                throw new IllegalStateException("Memory circuit not found");
+//            }
+//
+//            CircuitBoard cpu = circuitBoards.get("CPU");
+//            if(cpu == null) {
+//                throw new IllegalStateException("CPU circuit not found");
+//            }
+//
+//            // Find the Memory Subcircuit inside the CPU circuit
+//            Optional<Subcircuit> memorySubcircuitOptional =
+//                    cpu.getComponents().stream()
+//                            .filter(c -> c instanceof SubcircuitPeer)
+//                            .map(c -> ((SubcircuitPeer)c).getComponent())
+//                            .filter(s -> s.getSubcircuit() == memory.getCircuit())
+//                            .findFirst();
+//
+//            if(!memorySubcircuitOptional.isPresent()) {
+//                throw new IllegalStateException("RAM subcircuit not found");
+//            }
+//
+//            Subcircuit memorySubcircuit = memorySubcircuitOptional.get();
+//
+//            // Get its internal state, with the parent state being the CPU's top-level state
+//            CircuitState subcircuitState = memorySubcircuit.getSubcircuitState(
+//                    cpu.getCircuit().getTopLevelState());
+//
+//            // Find the RAM component inside the Memory circuit
+//            Optional<RAM> ramOptional = memory.getComponents().stream()
+//                    .filter(c -> c instanceof RAMPeer)
+//                    .map(c -> ((RAMPeer)c).getComponent())
+//                    .findFirst();
+//
+//            if(!ramOptional.isPresent()) {
+//                throw new IllegalStateException("RAM component not found");
+//            }
+//
+//            RAM ram = ramOptional.get();
+//
+//            // Now we can store
+//            ram.store(subcircuitState, 0x0000, 0x1234);
+//
+//            // And load
+//            System.out.println(ram.load(subcircuitState, 0x0000) == 0x1234);
+        } catch(Exception exc) {
+            exc.printStackTrace();
+        }
+
+        System.exit(0);
+    }
+}

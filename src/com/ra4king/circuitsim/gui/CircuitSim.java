c3af61e896590dda97f897cdca094eeda6ea8300
Roi Atalla <roi@roiatalla.com>
2018-06-18 04:13:05 -0700

    Bump to 1.7.4

--- src/com/ra4king/circuitsim/gui/CircuitSim.java
+++ src/com/ra4king/circuitsim/gui/CircuitSim.java
@@ -126,7 +126,7 @@ import javafx.util.Pair;
  * @author Roi Atalla
  */
 public class CircuitSim extends Application {
-	public static final String VERSION = "1.7.3";
+	public static final String VERSION = "1.7.4";
 	
 	private static boolean mainCalled = false;
 	private static AtomicBoolean versionChecked = new AtomicBoolean(false);

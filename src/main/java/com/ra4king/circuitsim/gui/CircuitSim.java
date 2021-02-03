13daa5f365e43df7e26c44ee1de5e19f20ed2077
Nicole Prindle <nprindle18@gmail.com>
2021-02-02 21:41:41 -0500

    Change Load -> Open to reduce confusion
    (cherry picked from commit 99cf3b07930e3408ec46a526ca6d32f564be1c7e)

--- src/main/java/com/ra4king/circuitsim/gui/CircuitSim.java
+++ src/main/java/com/ra4king/circuitsim/gui/CircuitSim.java
@@ -2135,9 +2135,9 @@ public class CircuitSim extends Application {
 			}
 		});
 		
-		MenuItem load = new MenuItem("Load");
-		load.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN));
-		load.setOnAction(event -> {
+		MenuItem open = new MenuItem("Open");
+		open.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN));
+		open.setOnAction(event -> {
 			if (checkUnsavedChanges()) {
 				return;
 			}
@@ -2279,7 +2279,7 @@ public class CircuitSim extends Application {
 			.addAll(newInstance,
 			        clear,
 			        new SeparatorMenuItem(),
-			        load,
+			        open,
 			        save,
 			        saveAs,
 			        print,
